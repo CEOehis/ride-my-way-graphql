418755e2eaa4a98adb706665500a1cf98b099845
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2018-12-28 13:29:49 +0100

    chore(postgres) setup postgereql database
    - setup models, migrations
    - use sequelize as ORM
    
    [finishes #1]

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'postgres'
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-const { GraphQLServer } = require('graphql-yoga');
+import { GraphQLServer } from 'graphql-yoga';
+import db from './database/models';
 
 const typeDefs = `
 type Query {
   info: String!
+  getUsers: [User!]
+}
+
+type User {
+  id: ID!
 }
 `;
 
 const resolvers = {
   Query: {
     info: () => 'This is the GraphQL API for Ride My Way',
+
+    getUsers: async () => {
+      const users = await db.User.findAll();
+      return users;
+    }
   }
 };
 
