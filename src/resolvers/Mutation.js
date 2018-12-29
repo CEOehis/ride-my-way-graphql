f5bbaa20dfe3e60c261d5d5ae7c2b9646bc7d888
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2018-12-29 19:05:18 +0100

    feat(login): enable authentication via login
    - implement sign in mutation
    
    [closes #3]

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,33 @@
 module.exports = {
-  signUp: async function signUp(parent, args, { models }, info) {
-    const { username, fullName, email, password } = args
+  signUp: async function signUp(parent, args, { models }) {
+    const { username, fullName, email, password } = args;
     const user = await models.User.create({
       username,
       fullName,
       email,
       password,
     });
+
+    return user;
+  },
+
+  signIn: async function signIn(parent, args, { models }) {
+    const { email, password } = args;
+    const user = await models.User.findOne({
+      where: {
+        email,
+      }
+    });
+
+    if(!user) {
+      throw new Error('Invalid login or password');
+    }
+
+    const isValid = await user.validatePassword(password);
+
+    if(!isValid) {
+      throw new Error('Invalid login or password');
+    }
 
     return user;
   }
